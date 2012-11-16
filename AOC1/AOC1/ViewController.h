42f68ecfb753424010ab33b2819ab77ebac1c66f
William Saults <wsaults@email.itt-tech.edu>
2012-11-15 21:43:22 -0600

    Just warming up

--- AOC1/AOC1/ViewController.h
+++ AOC1/AOC1/ViewController.h
@@ -1,9 +1,9 @@
 //
 //  ViewController.h
-//  AOC1
+//  AOC1 1212
 //
 //  Created by Will Saults on 11/15/12.
-//  Copyright (c) 2012 UTVCA. All rights reserved.
+//  Copyright (c) 2012 Full Sail. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- AOC1/AOC1/ViewController.m
+++ AOC1/AOC1/ViewController.m
@@ -1,12 +1,14 @@
 //
 //  ViewController.m
-//  AOC1
+//  AOC1 1212
 //
 //  Created by Will Saults on 11/15/12.
-//  Copyright (c) 2012 UTVCA. All rights reserved.
+//  Copyright (c) 2012 Full Sail. All rights reserved.
 //
 
 #import "ViewController.h"
+
+// TODO: cells have a width of 238 which is 1024 - 40 = 984 - (3 * 10) =  954 / 4 = 238.5
 
 @interface ViewController ()
 
