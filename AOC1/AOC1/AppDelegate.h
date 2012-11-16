466bf5757055917f6121ec45ab6cabdfeb83f363
William Saults <wsaults@email.itt-tech.edu>
2012-11-15 20:16:24 -0600

    Project made… Not wasting any time.

---
+++ AOC1/AOC1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AOC1
+//
+//  Created by Will Saults on 11/15/12.
+//  Copyright (c) 2012 UTVCA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AOC1/AOC1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AOC1
+//
+//  Created by Will Saults on 11/15/12.
+//  Copyright (c) 2012 UTVCA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
