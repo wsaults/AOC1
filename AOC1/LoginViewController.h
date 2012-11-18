01ccf6719170adc06b2cc14f6d5ef5fff8b1d1d1
William Saults <wsaults@email.itt-tech.edu>
2012-11-18 11:31:53 -0600

    Added the loginViewController while jamming to spotify.

---
+++ AOC1/LoginViewController.h
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.h
+//  AOC1
+//
+//  Created by Will Saults on 11/18/12.
+//  Copyright (c) 2012 UTVCA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController {
+    
+}
+
+-(IBAction)loginWithFacebook:(id)sender;
+-(IBAction)loginWithTwitter:(id)sender;
+-(IBAction)loginWithSpotify:(id)sender;
+-(IBAction)justShowMeTheAssignment:(id)sender;
+
+@end
---
+++ AOC1/LoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.m
+//  AOC1
+//
+//  Created by Will Saults on 11/18/12.
+//  Copyright (c) 2012 UTVCA. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
